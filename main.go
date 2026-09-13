6500cba7c6d19b9554c08314453d68de0c4ad138
codingrebahan <235741954+codingrebahan@users.noreply.github.com>
2026-09-13 22:31:15 +0700

    docs : move server comments inline

--- main.go
+++ main.go
@@ -18,11 +18,8 @@ func main() {
 	//cetak informasi web server berjalan di localhost
 	fmt.Println("Server berjalan di http://localhost:8080")
 
-	// Menjalankan server localhost di port 8080
-	err := http.ListenAndServe(":8080", nil)
-
-	//lakukan erro handling jika server tidak jalan berikan pesan
-	if err != nil {
+	err := http.ListenAndServe(":8080", nil) // Menjalankan server localhost di port 8080
+	if err != nil {                          //lakukan erro handling jika server tidak jalan berikan pesan
 		fmt.Println(err)
 	}
 }

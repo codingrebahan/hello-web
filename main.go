ee78fb29058447381a014fc150db4e91c3f279d0
codingrebahan <235741954+codingrebahan@users.noreply.github.com>
2026-09-13 22:06:07 +0700

    docs : menambahkan comment

--- main.go
+++ main.go
@@ -5,15 +5,23 @@ import (
 	"net/http"
 )
 
+// buat handlernya untuk mencetak hellow world
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello World")
 }
 
+// buat fungsi main
 func main() {
+	//regist helloHandler ke path "/" handlerfunc
 	http.HandleFunc("/", helloHandler)
 
+	//cetak informasi web server berjalan di localhost
 	fmt.Println("Server berjalan di http://localhost:8080")
+
+	// Menjalankan server localhost di port 8080
 	err := http.ListenAndServe(":8080", nil)
+
+	//lakukan erro handling jika server tidak jalan berikan pesan
 	if err != nil {
 		fmt.Println(err)
 	}

4941e0338be4acbfe38c5b5bc89819194f32ab90
codingrebahan <235741954+codingrebahan@users.noreply.github.com>
2026-09-15 19:08:29 +0700

    feat:add name query parameter

--- main.go
+++ main.go
@@ -7,13 +7,21 @@ import (
 
 // buat handlernya untuk mencetak hellow world
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World")
+	//deklarasi dan inisialisasi variable name dari query parameter
+	name := r.URL.Query().Get("name")
+
+	// Jika name kosong, gunakan "Guest" sebagai nilai default
+	if name == "" {
+		name = "Guest"
+	}
+	fmt.Fprintln(w, "hello", name)
 }
 
 // buat fungsi main
 func main() {
+
 	//regist helloHandler ke path "/" handlerfunc
-	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/hello", helloHandler)
 
 	//cetak informasi web server berjalan di localhost
 	fmt.Println("Server berjalan di http://localhost:8080")

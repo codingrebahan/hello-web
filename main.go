aaee7df790183b591acd83d51f826bc4ea62bb72
codingrebahan <235741954+codingrebahan@users.noreply.github.com>
2026-09-16 21:10:44 +0700

    feat: add HTTP method handling

--- main.go
+++ main.go
@@ -2,19 +2,36 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
 // buat handlernya untuk mencetak hellow world
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	//deklarasi dan inisialisasi variable name dari query parameter
-	name := r.URL.Query().Get("name")
 
-	// Jika name kosong, gunakan "Guest" sebagai nilai default
-	if name == "" {
-		name = "Guest"
+	switch r.Method {
+	case http.MethodGet:
+		//deklarasi dan inisialisasi variable name dari query parameter
+		name := r.URL.Query().Get("name")
+
+		// Jika name kosong, gunakan "Guest" sebagai nilai default
+		if name == "" {
+			name = "Guest"
+		}
+		fmt.Fprintln(w, "hello", name)
+	case http.MethodPost:
+		body, err := io.ReadAll(r.Body)
+
+		if err != nil {
+			fmt.Fprintln(w, "Error!", err)
+			return
+		}
+		fmt.Fprintln(w, string(body))
+	default:
+		fmt.Fprintln(w, "Hanya menerima GET dan POST")
+
 	}
-	fmt.Fprintln(w, "hello", name)
+
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
